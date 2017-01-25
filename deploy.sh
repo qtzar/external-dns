7fec21cd851e018cd20576cfd22404f1f5599877
dlynch <dlynch@czarnowski.com>
2017-01-25 15:29:25 -0500

    added gitIgnore

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-./scripts/build
-cd bin/
-tar cf ../packaging/external-dns.tar external-dns
-gzip -f9 ../packaging/external-dns.tar
-docker build -t qtzar/external-dns ../packaging
-docker push qtzar/external-dns
